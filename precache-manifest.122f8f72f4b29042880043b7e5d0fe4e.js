6e3655fd10d54c1fd1a5fa4b191adaafae8873fb
Alex Mercy <hambiru@yandex.ru>
2022-05-03 23:35:52 +0300

    Updates

---
+++ precache-manifest.122f8f72f4b29042880043b7e5d0fe4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caf97d20dad7e49e00f5cab43854d23f",
+    "url": "/Sector.business-test/index.html"
+  },
+  {
+    "revision": "fb49fd345b722ac87295",
+    "url": "/Sector.business-test/static/css/main.5569b6e6.chunk.css"
+  },
+  {
+    "revision": "3627444e555aa165858c",
+    "url": "/Sector.business-test/static/js/2.926e53e6.chunk.js"
+  },
+  {
+    "revision": "fb49fd345b722ac87295",
+    "url": "/Sector.business-test/static/js/main.0a2cf9bd.chunk.js"
+  },
+  {
+    "revision": "a665379f556754bd2d13",
+    "url": "/Sector.business-test/static/js/runtime-main.0e67e058.js"
+  },
+  {
+    "revision": "3fdbc9402621d938deec802123a7b6ad",
+    "url": "/Sector.business-test/static/media/Preloader.3fdbc940.gif"
+  }
+]);
